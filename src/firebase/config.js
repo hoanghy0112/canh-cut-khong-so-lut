547a538f44170354d2eccb058adf84518da0ffc5
hoanghy <hoanghy01122003@gmail.com>
2022-11-25 18:23:24 +0700

    create profile information hooks

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBscBycJQJgiq6ecVmxpDKmuJvYwCvwUeg",
+	authDomain: "penguinnoafraidflood.firebaseapp.com",
+	projectId: "penguinnoafraidflood",
+	storageBucket: "penguinnoafraidflood.appspot.com",
+	messagingSenderId: "726192918955",
+	appId: "1:726192918955:web:ffa67e0ee294518ed542ee",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
