2baf3a119eece086552a672420b5b04b208475e1
hoanghy <hoanghy01122003@gmail.com>
2022-11-25 21:58:36 +0700

    fix bug can't build

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MainRouter from "./routers/mainRouter";
+import MainRouter from "./routers/MainRouter";
 
 import "./index.css";
 
