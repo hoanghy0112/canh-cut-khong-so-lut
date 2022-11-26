25aa4a1c690cd488873857908c5c2855e0d74604
NTKhang <21520963@gm.uit.edu.vn>
2022-11-26 19:54:28 +0700

    update suggestForDay

--- src/routers/MainRouter.jsx
+++ src/routers/MainRouter.jsx
@@ -1,48 +1,48 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AuthenticationPage from "../pages/AuthenticationPage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthenticationPage from '../pages/AuthenticationPage';
 // import Info from '../components/UserProfile/Info';
-import HomePage from "../pages/HomePage/HomePage";
-import ItemPage from "../pages/ItemPage/ItemPage";
-import SuggestionPage from "../pages/SuggestionPage/SuggestionPage";
-import UsedPlasticPage from "../pages/UsedPlasticPage/UsedPlasticPage";
+// import HomePage from '../pages/HomePage/HomePage';
+// import ItemPage from '../pages/ItemPage/ItemPage';
+import SuggestionPage from '../pages/SuggestionPage/SuggestionPage';
+// import UsedPlasticPage from '../pages/UsedPlasticPage/UsedPlasticPage';
 
-import QDTestPage from "../pages/QDTstPage";
-import TestPage from "../pages/TestPage_khang/khangTest";
-import SchedulePage from "../pages/SchedulePage/SchedulePage";
+import QDTestPage from '../pages/QDTstPage';
+import TestPage from '../pages/TestPage_khang/khangTest';
+// import SchedulePage from '../pages/SchedulePage/SchedulePage';
 
 const router = createBrowserRouter([
-	{
-		path: "",
-		element: <HomePage />,
-		children: [
-			{
-				path: "item",
-				element: <ItemPage />,
-			},
-			{
-				path: "home",
-				element: <SchedulePage />,
-			},
-			{
-				path: "suggestion",
-				element: <SuggestionPage />,
-			},
-			{
-				path: "usedplastic",
-				element: <UsedPlasticPage />,
-			},
-		],
-	},
-	{
-		path: "authentication",
-		element: <AuthenticationPage />,
-	},
-	{
-		path: "test",
-		element: <TestPage />,
-	},
+  //   {
+  //     path: '',
+  //     // element: <HomePage />,
+  //     children: [
+  //       {
+  //         path: 'item',
+  //         element: <ItemPage />,
+  //       },
+  //       {
+  //         path: 'home',
+  //         element: <SchedulePage />,
+  //       },
+  //       {
+  //         path: 'suggestion',
+  //         element: <SuggestionPage />,
+  //       },
+  //       {
+  //         path: 'usedplastic',
+  //         element: <UsedPlasticPage />,
+  //       },
+  //     ],
+  //   },
+  {
+    path: 'authentication',
+    element: <AuthenticationPage />,
+  },
+  {
+    path: 'test',
+    element: <TestPage />,
+  },
 ]);
 
 export default function MainRouter() {
-	return <RouterProvider router={router} />;
+  return <RouterProvider router={router} />;
 }
