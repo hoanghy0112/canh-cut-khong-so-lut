12361f61c1c93b6b0ec351f4b653cd31af45aa9d
hoanghy <hoanghy01122003@gmail.com>
2022-11-26 09:42:17 +0700

    update

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -6,32 +6,13 @@ import { db } from "../firebase/config";
 import signInWithGoogle from "../firebase/signIn/signInWithGoogle";
 
 export default function AuthenticationPage() {
-	useEffect(() => {
-		async function testFirestore() {
-			try {
-				// const docRef = await addDoc(collection(db, "users"), {
-				// 	first: "Ada",
-				// 	last: "Lovelace",
-				// 	born: 1815,
-				// });
-				await setDoc(doc(db, "cities", "LA"), {
-					name: "Los Angeles",
-					state: "CA",
-					country: "USA",
-				});
-				console.log("aaaaaaaa");
-				// console.log("Document written with ID: ", docRef);
-			} catch (e) {
-				// console.error("Error adding document: ", e);
-			}
-		}
-
-		testFirestore();
-	}, []);
-
 	return (
 		<>
-			<button onClick={() => signInWithGoogle()}>Sign in with google</button>
+			<abbr title="Sign in with popup by Google account">
+				<button onClick={() => signInWithGoogle()}>
+					Sign in with google
+				</button>
+			</abbr>
 		</>
 	);
 }
