258e5c1b69f1bced84d42e2a70e49624e4a05288
NTKhang <21520963@gm.uit.edu.vn>
2022-11-27 02:04:45 +0700

    update sponsorPage

--- src/pages/SponsorPage/SponsorPage.jsx
+++ src/pages/SponsorPage/SponsorPage.jsx
@@ -62,7 +62,48 @@ const SponsorPage = () => {
           <p>App</p>
         </div>
         <div className={styles.outlet}>
-          <p className={styles.title}>Create new event</p>
+          <div className={styles.titleContainer}>
+            <p className={styles.title}>Create new event</p>
+            <div className={styles.inputContainer}>
+              <div className={styles.nameContainer}>
+                <p className={styles.title}>Title</p>
+                <input type='text' className={styles.input} />
+              </div>
+              <div className={styles.descriptionContainer}>
+                <p className={styles.description}>Description</p>
+                <textarea type='text' className={styles.input} />
+              </div>
+              <div className={styles.posterContainer}>
+                <p className={styles.poster}>Poster</p>
+                <input type='text' className={styles.input} />
+              </div>
+            </div>
+          </div>
+          <div className={styles.detailContainer}>
+            <div className={styles.detail}>
+              <div className={styles.border}>
+                <div className={styles.timePlace}>
+                  <div className={styles.takePlace}>
+                    <p className={styles.place}>Take place at</p>
+                    <input type='text' className={styles.input} />
+                  </div>
+                  <div className={styles.onContainer}>
+                    <p className={styles.on}>On</p>
+                    <input type='text' className={styles.input} />
+                  </div>
+                </div>
+                <div className={styles.endAt}>
+                  <p className={styles.end}>End at</p>
+                  <input type='text' className={styles.input} />
+                </div>
+                <div className={styles.At}>
+                  <p className={styles.container}>At</p>
+                  <input type='text' className={styles.input} />
+                </div>
+              </div>
+            </div>
+            <button className={styles.button}>Create</button>
+          </div>
         </div>
       </div>
     </div>
