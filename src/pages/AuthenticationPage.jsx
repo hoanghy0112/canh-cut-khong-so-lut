e4217291685875a9cc3b2346a6af6cae79a65f5b
hoanghy <hoanghy01122003@gmail.com>
2022-11-26 11:12:37 +0700

    update

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -4,13 +4,29 @@ import { collection, addDoc, doc, setDoc } from "firebase/firestore";
 
 import { db } from "../firebase/config";
 import signInWithGoogle from "../firebase/signIn/signInWithGoogle";
+import { Item } from "../firebase/model/Item";
+import { Material } from "../firebase/model/Material";
 
 export default function AuthenticationPage() {
+	const item = new Item("chen", 100, "nhua");
+	const material = new Material("nhua", 50);
+
+	useEffect(() => {
+		async function display() {
+			console.log(await item.getPolutionAmount());
+		}
+		display();
+	}, []);
+
 	return (
 		<>
 			<abbr title="Sign in with popup by Google account">
 				<button onClick={() => signInWithGoogle()}>
 					Sign in with google
+				</button>
+				<button onClick={() => item.saveToFirestore()}>save item</button>
+				<button onClick={() => material.saveToFirestore()}>
+					save material
 				</button>
 			</abbr>
 		</>
