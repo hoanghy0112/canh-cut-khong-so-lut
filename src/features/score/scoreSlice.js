406558c193a8ae94092ba725d474224bfa2eb369
NTKhang <21520963@gm.uit.edu.vn>
2022-11-27 03:08:10 +0700

    update scoreSlice

--- src/features/score/scoreSlice.js
+++ src/features/score/scoreSlice.js
@@ -1,31 +1,31 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-param-reassign */
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-	score: 0,
+  score: 0,
 };
 
 export const scoreManagementSlice = createSlice({
-	name: "scoreManagement",
-	initialState,
-	reducers: {
-		addScore: (state, action) => {
-			const score = action.payload;
+  name: 'scoreManagement',
+  initialState,
+  reducers: {
+    addScore: (state, action) => {
+      const score = action.payload;
 
-			state.point += score;
-		},
-		reduceScore: (state, action) => {
-			const score = action.payload;
+      state.score += score;
+    },
+    reduceScore: (state, action) => {
+      const score = action.payload;
 
-			state.point -= score;
-		},
-	},
+      state.score -= score;
+    },
+  },
 });
 
 export const { addScore, reduceScore } = scoreManagementSlice.actions;
 
-export const selectScore = (state) => state.scoreManagement.point;
+export const selectScore = (state) => state.scoreManagement.score;
 
 export default scoreManagementSlice.reducer;
